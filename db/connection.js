8793631de1b88d728ce835c01a96493fa79ebb0d
Christopher <ctijerina@live.com>
2022-01-26 18:12:09 -0600

    all issues covered commit

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,10 @@ const mysql = require("mysql2");
 const db = mysql.createConnection(
     {
         host: "localhost",
-        // Your MySQL username,
-        user: "root",
-        // Your MySQL password
-        password: "%0<wVIgL%i33",
+        // Your MySQL username,temporary user
+        user: "newuser",
+        // Your MySQL password, temporary password
+        password: "password",
         database: "election"
     },
     console.log("Connected to the election database.")
